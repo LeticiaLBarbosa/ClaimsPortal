0fb23dede35ffa3c157f5d1a1161be9a72eb6a05
Leticia Barbosa <mleonciobarbosa2764@conestogac.on.ca>
2021-05-29 19:26:31 -0400

    Adding inital structure to register a new user

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,33 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    accountStatus: "Create New Account",
+    form: {
+      firstName: undefined,
+      lastName: undefined,
+      accountNumber: undefined,
+      taxId: undefined,
+      address: undefined,
+    },
+  },
+  mutations: {
+    accountStatus(state, accountStatus) {
+      state.accountStatus = accountStatus;
+    },
+    form(state, form) {
+      state.form = form;
+    },
+  },
+  actions: {
+    async getMessage({ commit, state }) {
+      if (!state.accountStatus) {
+        const status = {
+          accountStatus: "Create New Account",
+        };
+        commit("message", status);
+      }
+    },
+  },
   modules: {},
 });
---
+++ src/views/RegisterUser.vue
@@ -0,0 +1,94 @@
+<template>
+  <b-container>
+    <h1>{{ $store.state.accountStatus }}</h1>
+    <b-form-container></b-form-container>
+    <b-row>
+      <label for="firstName">First name: </label>
+      <input
+        v-model="form.firstName"
+        type="text"
+        id="firstName"
+        placeholder="Please enter your first name"
+      />
+    </b-row>
+    <b-row>
+      <label for="lastName">Last name: </label>
+      <input
+        v-model="form.lastName"
+        type="text"
+        id="lastName"
+        placeholder="Please enter your last name"
+      />
+    </b-row>
+    <b-row>
+      <label for="accountNumber">Account Number: </label>
+      <input
+        v-model="form.accountNumber"
+        type="text"
+        id="accountNumber"
+        placeholder="Please enter your account number"
+      />
+    </b-row>
+    <b-row>
+      <label for="accountNumber">Tax ID: </label>
+      <input
+        v-model="form.accountNumber"
+        type="text"
+        id="accountNumber"
+        placeholder="Please enter your tax ID"
+      />
+    </b-row>
+    <b-row>
+      <label for="address">Address: </label>
+      <input
+        v-model="form.address"
+        type="text"
+        id="address"
+        placeholder="Please enter your address"
+      />
+    </b-row>
+    <div>
+      <b-button v-b-modal.modal-1>Registration Confirmation</b-button>
+      <b-modal id="modal-1" title="BootstrapVue">
+        <p class="my-4">{{ this.form.firstName }}</p>
+        <p class="my-4">{{ this.form.lastName }}</p>
+        <p class="my-4">{{ this.form.accountNumber }}</p>
+        <p class="my-4">{{ this.form.taxId }}</p>
+        <p class="my-4">{{ this.form.address }}</p>
+      </b-modal>
+    </div>
+  </b-container>
+</template>
+<script>
+export default {
+  name: "Account",
+  data() {
+    return {
+      status: undefined,
+      form: {
+        firstName: undefined,
+        lastName: undefined,
+        accountNumber: undefined,
+        taxId: undefined,
+        address: undefined,
+      },
+    };
+  },
+  computed: {},
+  async mounted() {
+    await this.$store.dispatch("getMessage");
+    this.status = Object.assign({}, this.$store.state.accountStatus);
+  },
+  methods: {
+    async save() {
+      this.form.firstName = this.form.firstName.trim();
+      this.form.lastName = this.form.lastName.trim();
+      this.form.accountNumber = this.accountNumber;
+      this.form.taxId = this.taxId;
+      this.form.address = this.address;
+      this.$store.commit("form", this.form);
+      this.form = Object.assign({}, this.$store.state.form);
+    },
+  },
+};
+</script>
