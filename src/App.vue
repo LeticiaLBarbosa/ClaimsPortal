50802424bf54eab22f0cb2372e7312e52c9629c6
Leticia Barbosa <mleonciobarbosa2764@conestogac.on.ca>
2021-05-30 17:14:52 -0400

    Adjustments on mai application

--- src/App.vue
+++ src/App.vue
@@ -12,9 +12,6 @@ export default {
   components: {
     Navbar,
   },
-  async created() {
-    await this.$store.dispatch("tryLogin");
-  },
 };
 </script>
 <style>
@@ -22,15 +19,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
   color: #2c3e50;
 }
 
